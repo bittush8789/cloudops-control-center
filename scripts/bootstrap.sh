187dffc9f9c3aa1bfbdb657abdcf2483e0cbf61a
Bittu Sharma <bittush9534@gmail.com>
2026-04-25 14:45:44 +0530

    feat: implement advanced rollout strategies (Canary, Blue/Green) using Argo Rollouts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,16 +14,21 @@ echo "⚓ Adding Helm Repositories..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add argo-rollouts https://argoproj.github.io/argo-helm
 helm repo update
 
 # 3. Create Namespaces
 echo "📁 Creating Kubernetes Namespaces..."
 kubectl create namespace argocd || true
+kubectl create namespace argo-rollouts || true
 kubectl create namespace cloudops-dev || true
 kubectl create namespace cloudops-prod || true
 
-# 4. Install ArgoCD
+# 4. Install ArgoCD & Rollouts
 echo "🐙 Installing ArgoCD..."
 helm upgrade --install argocd argo/argo-cd --namespace argocd
+
+echo "🌀 Installing Argo Rollouts..."
+helm upgrade --install argo-rollouts argo-rollouts/argo-rollouts --namespace argo-rollouts
 
 echo "✅ Bootstrap Complete!"
